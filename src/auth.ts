2cebd604e936bef776ef18d82347c47aa02d460a
Julius Kiano <jgkiano@gmail.com>
2020-04-12 22:43:42 +0300

    created fetchUserVerificationToken function

--- src/auth.ts
+++ src/auth.ts
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import {
   OkHiAppContext,
   OkHiMode,
@@ -8,12 +9,23 @@ import {
 const DEV_AUTH_URL =
   'https://dev-api.okhi.io/v5/auth/mobile/generate-auth-token';
 
-const PROD_AUTH_URL = 'https://api.okhi.io/v5/auth/mobile/generate-auth-token';
-
 const SANDBOX_AUTH_URL =
   'https://sandbox-api.okhi.io/v5/auth/mobile/generate-auth-token';
 
+const PROD_AUTH_URL = 'https://api.okhi.io/v5/auth/mobile/generate-auth-token';
+
+const DEV_USER_VERIFY_TOKEN_URL_PREFIX =
+  'https://dev-api.okhi.io/v5/auth/verification-token';
+
+const SANDBOX_USER_VERIFY_TOKEN_URL_PREFIX =
+  'https://sandbox-api.okhi.io/v5/auth/verification-token';
+
+const PROD_USER_VERIFY_TOKEN_URL_PREFIX =
+  'https://api.okhi.io/v5/auth/verification-token';
+
 let AUTHORIZATION_URL: string;
+
+let USER_VERIFY_TOKEN_URL_PREFIX: string;
 
 let ctxt: OkHiContext = {
   developer: {
@@ -29,26 +41,57 @@ let ACCESS_TOKEN: string;
 
 let AUTHORIZATION_TOKEN: string;
 
+export const fetchUserVerificationToken = async (userId: string) => {
+  try {
+    if (typeof userId !== 'string') {
+      throw new Error('invalid user id');
+    }
+    if (
+      typeof ACCESS_TOKEN !== 'string' ||
+      typeof USER_VERIFY_TOKEN_URL_PREFIX !== 'string'
+    ) {
+      throw new Error('invalid init config provided');
+    }
+    const { data } = await axios.get<{ authorization_token: string }>(
+      `${USER_VERIFY_TOKEN_URL_PREFIX}?user-id=${userId}`,
+      {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: ACCESS_TOKEN,
+        },
+      }
+    );
+    if (!data.authorization_token) {
+      throw new Error('verification_token not provided');
+    }
+    return data.authorization_token;
+  } catch (error) {
+    throw error;
+  }
+};
+
 export const fetchAuthorizationToken = async () => {
   try {
     if (AUTHORIZATION_TOKEN) {
       return AUTHORIZATION_TOKEN;
     }
-    if (!ACCESS_TOKEN) {
-      throw new Error('invalid access token provided');
+    if (
+      typeof ACCESS_TOKEN !== 'string' ||
+      typeof AUTHORIZATION_URL !== 'string'
+    ) {
+      throw new Error('invalid init config provided');
     }
-    const response = await fetch(AUTHORIZATION_URL, {
-      method: 'GET',
-      headers: {
-        Accept: 'application/json',
-        'Content-Type': 'application/json',
-        Authorization: ACCESS_TOKEN,
-      },
-    });
-    if (response.status !== 200) {
-      throw new Error('invalid auth token');
-    }
-    const data: { authorization_token: string } = await response.json();
+    const { data } = await axios.get<{ authorization_token: string }>(
+      AUTHORIZATION_URL,
+      {
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          Authorization: ACCESS_TOKEN,
+        },
+      }
+    );
     if (!data.authorization_token) {
       throw new Error('authorization_token not provided');
     }
@@ -76,14 +119,17 @@ export const init = (auth: string, context: OkHiAppContext) => {
   // define endpoints
   if (ctxt.mode === OkHiMode.DEV) {
     AUTHORIZATION_URL = DEV_AUTH_URL;
+    USER_VERIFY_TOKEN_URL_PREFIX = DEV_USER_VERIFY_TOKEN_URL_PREFIX;
   }
 
   if (ctxt.mode === OkHiMode.SANDBOX) {
     AUTHORIZATION_URL = SANDBOX_AUTH_URL;
+    USER_VERIFY_TOKEN_URL_PREFIX = SANDBOX_USER_VERIFY_TOKEN_URL_PREFIX;
   }
 
   if (ctxt.mode === OkHiMode.PROD) {
     AUTHORIZATION_URL = PROD_AUTH_URL;
+    USER_VERIFY_TOKEN_URL_PREFIX = PROD_USER_VERIFY_TOKEN_URL_PREFIX;
   }
 
   fetchAuthorizationToken()
