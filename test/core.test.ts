ee77848f15eea09474e365ab35819cc6bc8106cf
Julius Kiano <jgkiano@gmail.com>
2020-04-13 11:09:28 +0300

    added core tests

---
+++ test/core.test.ts
@@ -0,0 +1,24 @@
+import OkHiCore, { OkHiCoreConfiguration, OkHiMode } from '../src';
+
+describe('okhi core dev tests', () => {
+  const configuration: OkHiCoreConfiguration = {
+    auth: 'SWF0ejlENkFOVDphZjNkZGQxMi00ZTI5LTQ1MDItODQyMS1iZTlkNmUzODcwZTU=',
+    context: {
+      mode: OkHiMode.DEV,
+    },
+  };
+  const core = new OkHiCore(configuration);
+
+  it('fetches and authorization token', async () => {
+    const token = await core.fetchAuthorizationToken();
+    const isToken = token.startsWith('Token ');
+    expect(isToken).toBeTruthy();
+  });
+
+  it('fetches and user authorization token', async () => {
+    const userId = 'i3c5W92cB8';
+    const token = await core.fetchUserVerificationToken(userId);
+    const isToken = token.startsWith('Token ');
+    expect(isToken).toBeTruthy();
+  });
+});
